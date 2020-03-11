48f629a45fd7fadf46fb446947383ade03656e8d
Ravi Pratap Singh <Ravipratap.s@activa.one>
2020-03-11 18:41:16 +0530

    pre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './user/Pages/home/home.component';
+import { LoginComponent } from './user/Pages/login/login.component';
+import { RegisterComponent } from './user/Pages/register/register.component';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routeConfigure = [LoginComponent, RegisterComponent, HomeComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routeConfigure } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent,
-    UserComponent
+    routeConfigure
   ],
   imports: [
     BrowserModule,
